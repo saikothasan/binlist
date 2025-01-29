2a9d3a777aee2838287c2ffd39916eb24215d0f5
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-01-29 17:50:10 +0600

    Update not-found.tsx

--- app/bin/[bin]/not-found.tsx
+++ app/bin/[bin]/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="container flex flex-col items-center justify-center min-h-[calc(100vh-8rem)] text-center">
       <h1 className="text-4xl font-bold mb-4">BIN Not Found</h1>
-      <p className="text-xl mb-8">Sorry, we couldn't find any information for the requested BIN.</p>
+      <p className="text-xl mb-8">Sorry, we couldn&apos;t find any information for the requested BIN.</p>
       <Button asChild>
         <Link href="/bin-checker">Try Another BIN</Link>
       </Button>
