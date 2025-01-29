6f1ba079dfd3a6cd571823205a8061a55ff8ef52
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-01-29 17:37:43 +0600

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link"
-
 export default function Footer() {
   return (
     <footer className="border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
